bfe4cff6eeafe96e98a5b336349c511b3b4f399a
AlexGallardo <gallardonicolas.8100@outlook.com>
2024-09-27 12:58:20 -0300

    add font

---
+++ main.lua
@@ -0,0 +1,16 @@
+
+function love.load()
+
+   local init = require 'src.game.init'
+
+end
+
+
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
---
+++ src/game/init.lua
@@ -0,0 +1,5 @@
+function InitGame()
+    titleFont = love.graphics.newFont("/res/Playwrite/PlaywriteDEGrund-VariableFont_wght.ttf", 60)
+
+    love.graphics.setFont(titleFont)
+end
