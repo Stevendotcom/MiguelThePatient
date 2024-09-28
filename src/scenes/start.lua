3275866a894b1c3cbfc31dcb2f8c2ae40c4dba85
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-09-28 16:12:05 -0300

    add start scene

---
+++ src/scenes/start.lua
@@ -0,0 +1,4 @@
+function startScene()
+    title = love.graphics.newText(titleFont, "Miguel The Patient")
+    love.graphics.draw(title, love.graphics.getWidth()/2 - (title:getWidth())/2,love.graphics.getHeight()/2)
+end
