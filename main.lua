d954ed559392373a56e6edd278fc9edb6e52630f
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-09-28 17:12:34 -0300

    added some collisions

--- main.lua
+++ main.lua
@@ -1,10 +1,64 @@
+local screenWidth = love.graphics.getWidth()
+local screenHeight = love.graphics.getHeight()
+
+function drawDebug()
+   for _, body in pairs(world:getBodies()) do
+      for _, fixture in pairs(body:getFixtures()) do
+          local shape = fixture:getShape()
+
+          if shape:typeOf("CircleShape") then
+              local cx, cy = body:getWorldPoints(shape:getPoint())
+              love.graphics.circle("fill", cx, cy, shape:getRadius())
+          elseif shape:typeOf("PolygonShape") then
+              love.graphics.polygon("line", body:getWorldPoints(shape:getPoints()))
+          else
+              love.graphics.line(body:getWorldPoints(shape:getPoints()))
+          end
+      end
+  end
+end
+
+
+
+function makeGround()
+    objects.ground = {}
+    objects.ground.body =  love.physics.newBody(world, screenWidth/2, screenHeight - 45)
+    objects.ground.shape = love.physics.newRectangleShape(screenWidth, 90)
+    objects.ground.fixture = love.physics.newFixture(objects.ground.body, objects.ground.shape)
+end
+
+function makeWalls()
+
+    objects.wallLeft = {}
+    objects.wallLeft.body =  love.physics.newBody(world, 25, screenHeight/2 - 45)
+    objects.wallLeft.shape = love.physics.newRectangleShape(50, screenHeight - 88)
+    objects.wallLeft.fixture = love.physics.newFixture(objects.wallLeft.body, objects.wallLeft.shape)
+
+    objects.wallRight = {}
+    objects.wallRight.body =  love.physics.newBody(world, screenWidth - 25, screenHeight/2 - 45)
+    objects.wallRight.shape = love.physics.newRectangleShape(50, screenHeight - 88)
+    objects.wallRight.fixture = love.physics.newFixture(objects.wallRight.body, objects.wallRight.shape)
+
+end
+
+function makeRoof()
+    objects.roof = {}
+    objects.roof.body =  love.physics.newBody(world, screenWidth/2, 10)
+    objects.roof.shape = love.physics.newRectangleShape(screenWidth, 20)
+    objects.roof.fixture = love.physics.newFixture(objects.roof.body, objects.roof.shape)
+end
+
 
 function love.load()
 
    local init = require 'src.game.init'
    local ui = require 'src.game.ui'
+   local sceneManager = require 'src.game.sceneManager'
    local start = require 'src.scenes.start'
    InitGame()
+   makeGround()
+   makeWalls()
+   makeRoof()
 
 end
 
@@ -18,10 +72,12 @@ end
 
 function love.draw()
 
-   love.graphics.draw(background, 0, 0, 0, love.graphics.getWidth()/background:getWidth(), love.graphics.getHeight()/background:getHeight())
-
+   love.graphics.draw(background, 0, 0, 0, screenWidth/background:getWidth(),screenHeight/background:getHeight())
+   if selectedScene == start then
    startScene()
-
+   end
    drawUI()
 
+   drawDebug()
 end
+
