876b9193786f835990fcf05ae786de29789efa65
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-09-28 17:13:16 -0300

    add knight

---
+++ src/actors/knight.lua
@@ -0,0 +1,19 @@
+knight =
+{
+    pos = { 30, 30}
+    life = 100,
+    damage = 5,
+    actions =
+    {
+        run,
+        hit,
+        idle,
+        roll,
+        pray
+    },
+    state = knight.actions,
+}
+
+function input()
+
+end
