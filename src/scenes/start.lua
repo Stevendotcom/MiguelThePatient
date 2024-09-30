284d25f7766f7b90a5b25cced29990bc4bd93bce
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-09-30 11:49:48 -0300

    changed getWidth into variables for multiple use

--- src/scenes/start.lua
+++ src/scenes/start.lua
@@ -1,4 +1,9 @@
+
+local screenWidth = love.graphics.getWidth()
+local screenHeight = love.graphics.getHeight()
+
 function startScene()
     title = love.graphics.newText(titleFont, "Miguel The Patient")
-    love.graphics.draw(title, love.graphics.getWidth()/2 - (title:getWidth())/2,love.graphics.getHeight()/2)
+    love.graphics.draw(title, screenWidth/2 - (title:getWidth())/2,screenHeight/2)
 end
+
